77afa43b808026441df23a9842d11dce47b2cd4f
Sal Scotto <sal.scotto@gmail.com>
2020-01-21 08:33:26 -0700

    Updated api interface so an optioanl resovler can be passed that will recieve the ip address result based on mac address arp

--- spec/wol_spec.cr
+++ spec/wol_spec.cr
@@ -1,11 +1,11 @@
 require "./spec_helper"
 describe Wol do
   it "works" do
-	Wol.wake("48:8D:36:76:54:A2") {|x|     
-		x.should eq(true)
-	}
-	Wol.wake("48:8D:36:76:54:A2",{"num_packets" => 3, "port"=> 7}){|x|
-		x.should eq(true)
-	}
+	resolved = "no ip"
+	Wol.wake("48:8D:36:76:54:A0",resolver: ->(i : String) {
+		resolved = i
+	})
+	resolved.should eq("no ip")
+	Wol.wake("48:8D:36:76:54:A2",port: 7,num_packets: 3).should eq(true)
   end
 end
--- src/wol.cr
+++ src/wol.cr
@@ -4,23 +4,21 @@ require "socket"
 module Wol
   extend self
   VERSION = "0.1.0"
-  # Options:
+  # Args:
   #		address: String target address defaults to 255.255.255.255
   #		num_packets: Int32 number of packets to send (defauls to 3)
   #		interval: Int32 number of milliseconds between sends
   #		port: Int32 port to send message, defaults to 9 (7 could be a viable alternative)
   #		secure: password to generate a secure wol packet
-  # accepts a block
-  def wake(mac_address : String, opts : Hash(String,String | Int32 ) = {"address" => "255.255.255.255","num_packets"=>3,"interval"=>100,"port"=>9,"secure"=>""})
-		address = opts["address"]? || "255.255.255.255"
-		num_packets = (opts["num_packets"]? || 3).to_i
-		interval    = (opts["interval"]? || 100).to_i
-		port        = (opts["port"]? || 9).to_i
+  # 	resolver: if provided will be called once an ip can be resolved for the provided mac address.
+  def wake(mac_address : String, address : String = "255.255.255.255", num_packets = 3, interval = 100, port = 9, secure : String? = nil, resolver : Proc(String,_)? = nil)
 		magic_packet= create_magic_packet(mac_address)
-		if opts["secure"]? && opts["secure"].to_s.size == 12
-			magic_packet.write opts["secure"].to_s.hexbytes
+		if !secure.nil? && secure.size == 12
+			magic_packet.write secure.hexbytes
 		end	
+		
 		puts magic_packet.to_slice.hexdump
+		
 		socket = UDPSocket.new
 		begin
 			socket.connect(address.to_s,port)
@@ -29,10 +27,32 @@ module Wol
 			(0..(num_packets-1)).each do |i|
 				rc = socket.send magic_packet
 				sleep (interval/1000)
-			end
-			yield true
+			end			
+			if !resolver.nil?
+				command = "arp -a -n"
+				io = IO::Memory.new
+				Process.run(command, shell: true, output: io)
+				output = io.to_s
+				rows = output.split(/\n/)
+				table = Hash(String,String).new
+				rows.each do |r|					
+					parts = r.split(/\s/)
+					if parts.size > 2
+						ip = parts[1].gsub('(',' ').gsub(')',' ').strip
+						mac = parts[3].downcase
+						table[mac] = ip
+					end	
+				end	
+				resolver.call(table[mac_address.downcase]? || "no ip")
+			end	
+			return true
 		rescue ex : Exception
-			yield false
+			return false
+		ensure
+			begin
+				socket.close	
+			rescue
+			end		
 		end		
   end 
   # convert mac address to string
